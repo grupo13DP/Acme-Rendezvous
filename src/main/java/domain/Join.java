a9a04a3a90080185c011f6d3dac83f27713f877d
yacinty <yacinty@users.noreply.github.com>
2018-02-14 17:09:23 +0100

    Dominio de Join.

--- src/main/java/domain/Join.java
+++ src/main/java/domain/Join.java
@@ -1,10 +1,61 @@
 package domain;
 
-import javax.persistence.Access;
-import javax.persistence.AccessType;
-import javax.persistence.Entity;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import java.util.Date;
 
 @Entity
 @Access(AccessType.PROPERTY)
 public class Join extends DomainEntity {
+
+    // Constructors -----------------------------------------------------------
+
+    public Join() {
+        super();
+    }
+
+
+    // Attributes -------------------------------------------------------------
+    private Date moment;
+
+    @NotNull
+    @Past
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    // Relationships ----------------------------------------------------------
+
+    private User user;
+    private Rendezvous rendezvous;
+
+    @Valid
+    @ManyToOne(optional = false)
+    public User getUser() {
+        return user;
+    }
+
+    @Valid
+    @ManyToOne(optional = false)
+    public Rendezvous getRendezvous() {
+        return rendezvous;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setRendezvous(Rendezvous rendezvous) {
+        this.rendezvous = rendezvous;
+    }
 }
