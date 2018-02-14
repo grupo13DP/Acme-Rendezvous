6db3d0360f3d00f683d4989e204ad17ebd321461
yacinty <yacinty@users.noreply.github.com>
2018-02-14 12:05:21 +0100

    Dominio de Answer y Question.

--- src/main/java/domain/Answer.java
+++ src/main/java/domain/Answer.java
@@ -1,10 +1,36 @@
 package domain;
 
-import javax.persistence.Access;
-import javax.persistence.AccessType;
-import javax.persistence.Entity;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.SafeHtml;
+
+import javax.persistence.*;
+import javax.validation.Valid;
 
 @Entity
 @Access(AccessType.PROPERTY)
 public class Answer extends DomainEntity {
+
+    // Constructors -----------------------------------------------------------
+
+    public Answer() {
+        super();
+    }
+
+    // Attributes -------------------------------------------------------------
+
+    private String answer;
+
+    @SafeHtml
+    @NotBlank
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    // Relationships ----------------------------------------------------------
+
+
 }
--- src/main/java/domain/Question.java
+++ src/main/java/domain/Question.java
@@ -1,10 +1,61 @@
 package domain;
 
-import javax.persistence.Access;
-import javax.persistence.AccessType;
-import javax.persistence.Entity;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.SafeHtml;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
 
 @Entity
 @Access(AccessType.PROPERTY)
 public class Question extends DomainEntity {
+
+
+    // Constructors -----------------------------------------------------------
+
+    public Question() {
+        super();
+    }
+
+
+    // Attributes -------------------------------------------------------------
+    private String text;
+
+    @SafeHtml
+    @NotBlank
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+// Relationships ----------------------------------------------------------
+
+    private Collection<Answer> answers;
+    private RendezVous rendezVous;
+
+    @NotNull
+    @Valid
+    @OneToMany(cascade = CascadeType.ALL)
+    public Collection<Answer> getAnswers() {
+        return answers;
+    }
+
+    @Valid
+    @ManyToOne(optional = false)
+    public RendezVous getRendezVous() {
+        return rendezVous;
+    }
+
+    public void setAnswers(Collection<Answer> answers) {
+        this.answers = answers;
+    }
+
+    public void setRendezVous(RendezVous rendezVous) {
+        this.rendezVous = rendezVous;
+    }
 }
